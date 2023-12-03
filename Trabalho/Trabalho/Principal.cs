4859e0b8394920b3629e4fa0aecb6a4236571842
LMaverick <91562063+LMaverick@users.noreply.github.com>
2023-12-03 10:51:00 -0300

    up

--- Trabalho/Trabalho/Principal.cs
+++ Trabalho/Trabalho/Principal.cs
@@ -98,7 +98,7 @@ namespace Trabalho
             if (id == 1)
             {
                 string titulo = "Foco e Disciplina";
-                string[] sinopse = { "Como o foco e disciplina influência na vida de uma pessoa,", "muitas pessoas pensam e pensam que ter muito foco as ", " ajudariam a conquistar as melhores coisas da vida, o quanto ", "disso é verdade?" };
+                string[] sinopse = { "Como o foco e disciplina influência na vida de uma pessoa,", "muitas pessoas pensam e pensam que ter muito foco as ", "ajudariam a conquistar as melhores coisas da vida, o quanto ", "disso é verdade?" };
                 string[] categoria = { "Ação, Mistério, Aventura, Suspense" };
                 string[] capitulo = { "Capítulo 1: Preparando o ciclo de sofrimento", "Capítulo 2: Alex o Insuportável, Mia a Apaixonada ", "ne Arthur o Vidente", "Capítulo 3: Desastre natural", "Capítulo 4: O Fim mutável? E a mudança drástica!", "EXTRA: Alex o rei do mundo" };
 
@@ -118,9 +118,9 @@ namespace Trabalho
             {
 
                 string titulo = "CAVERNA DO DRAGÃO";
-                string[] sinopse = { "Seis jovens entre 10 e 18 anos, entram em um parque de diversões chamado Caverna do Dragão. Quando fazem um passeio em uma espécie de trem fantasma, são catapultados para um mundo paralelo, cheio de monstros, demônios e mágos. " };
+                string[] sinopse = { "Seis jovens entre 10 e 18 anos, entram em um parque de ", "diversões chamado Caverna do Dragão. Quando fazem um ", "passeio em uma espécie de trem fantasma, são catapultados para ", "um mundo paralelo, cheio de monstros, demônios e magos. " };
                 string[] categoria = { "Ação, Aventura, Magia, Exploração" };
-                string[] capitulo = { "" };
+                string[] capitulo = { "Capítulo 1: A Queda no Abismo Interdimensional",    "Capítulo 2: Encontros nas Terras Desconhecidas", "Capítulo 3: Desafios na Cidade dos Elfos", "Capítulo 4: Confronto com o Mestre dos Pesadelos" };
 
                 pictureBox1.Image = imageList1.Images[id];
                 txt_titulo.Text = titulo;
@@ -136,9 +136,9 @@ namespace Trabalho
             {
 
                 string titulo = "É assim que acaba - Colleen Hoover ";
-                string[] sinopse = { "Em É assim que acaba, Colleen Hoover nos apresenta Lily, uma jovem que se mudou de uma cidadezinha do Maine para Boston, se formou em marketing e abriu a própria floricultura. " };
+                string[] sinopse = { "Em É assim que acaba, Colleen Hoover nos apresenta Lily, ", "uma jovem que se mudou de uma cidadezinha do Maine ", "para Boston, se formou em marketing e abriu a própria","floricultura. " };
                 string[] categoria = { "Drama, Romance, BestSellers" };
-                string[] capitulo = { "" };
+                string[] capitulo = { "Capítulo 1: O Encontro Inesperado", "Capítulo 2: Laços que se Desfazem", "Capítulo 3: Entre Páginas e Promessas", "Capítulo 4: O Adeus Necessário" };
 
                 pictureBox1.Image = imageList1.Images[id];
                 txt_titulo.Text = titulo;
@@ -154,9 +154,9 @@ namespace Trabalho
             {
 
                 string titulo = "Entre as chamas sob a água";
-                string[] sinopse = { "Fala do seu mais recente romance, “Entre as Chamas, Sob a Água\", romance histórico que retrata a brutalidade da Guerra de Canudos" };
+                string[] sinopse = { "Fala do seu mais recente romance, “Entre as Chamas, Sob  ", "a Água, romance histórico que retrata a  ", "brutalidade da Guerra de Canudos" };
                 string[] categoria = { "Histórico, Drama, Romance, Guerra" };
-                string[] capitulo = { "" };
+                string[] capitulo = { "Capítulo 1: A Inexplorada Profundidade", "Capítulo 2: Mistérios nas Correntezas", "Capítulo 3: Confronto nas Cavernas Aquáticas", "Capítulo 4: O Segredo do Fogo Submerso" };
 
                 pictureBox1.Image = imageList1.Images[id];
                 txt_titulo.Text = titulo;
@@ -172,7 +172,7 @@ namespace Trabalho
             {
  
                 string titulo = "Eu Sei O Que Vocês Fizeram no Verão Passado";
-                string[] sinopse = { "Um ano após atropelar um pescador e jogar o seu corpo na água, quatro amigos se reúnem quando Julie recebe uma carta assustadora dizendo-lhe que seu crime foi visto. " };
+                string[] sinopse = { "Um ano após atropelar um pescador e jogar o seu corpo na ", "água, quatro amigos se reúnem quando Julie  ", "recebe uma carta assustadora dizendo-lhe que ","seu crime foi visto. " };
                 string[] categoria = { "Mistério, Assassinato, Drama" };
                 string[] capitulo = { "" };
 
@@ -190,7 +190,7 @@ namespace Trabalho
             {
 
                 string titulo = "Harry Potter e o Prisioneiro de Azkaban";
-                string[] sinopse = { "É o início do terceiro ano na escola de bruxaria Hogwarts. Harry, Ron e Hermione têm muito o que aprender. " };
+                string[] sinopse = { "É o início do terceiro ano na escola de bruxaria Hogwarts.", "Harry, Ron e Hermione têm muito o que aprender. " };
                 string[] categoria = { "Magia, Drama, Aventura" };
                 string[] capitulo = { "" };
 
@@ -208,7 +208,7 @@ namespace Trabalho
             {
                 
                 string titulo = "Medo imortal ";
-                string[] sinopse = { "São contos que evocam o sobrenatural, apresentam monstros, descrevem atos de psicopatas, dão o testemunho de todo tipo imaginável de atrocidades concebidas pela mente humana." };
+                string[] sinopse = { "São contos que evocam o sobrenatural, apresentam ", "monstros ,descrevem atos de psicopatas, dão o ", "testemunho de todo tipo imaginável de atrocidades ", "concebidas pela mente humana." };
                 string[] categoria = { "Terror, Suspense, Psicologico" };
                 string[] capitulo = { "" };
 
@@ -227,7 +227,7 @@ namespace Trabalho
             {
                 
                 string titulo = "O gato que amava livros";
-                string[] sinopse = { "O gato deve encontrar um verdadeiro amante dos livros para acompanhá-lo em uma missão." };
+                string[] sinopse = { "O gato deve encontrar um verdadeiro amante dos livros para", " acompanhá-lo em uma missão." };
                 string[] categoria = { "Aventura, Relaxante, Místico" };
                 string[] capitulo = { "" };
 
@@ -245,7 +245,7 @@ namespace Trabalho
             {
                 
                 string titulo = "Percy jackson e os olimpianos";
-                string[] sinopse = { "Um artefato precioso foi roubado do Monte Olimpo e Percy é o principal suspeito. " };
+                string[] sinopse = { "Um artefato precioso foi roubado do Monte Olimpo e Percy ", "é o principal suspeito. " };
                 string[] categoria = { "Mágico, Aventura, Ação" };
                 string[] capitulo = { "" };
 
@@ -263,7 +263,7 @@ namespace Trabalho
             {
                 
                 string titulo = "Todas as suas (im)perfeições";
-                string[] sinopse = { "Todas as suas (im)perfeições fala sobre um casamento conturbado e uma promessa esquecida que pode ser capaz de salvá-lo. " };
+                string[] sinopse = { "Todas as suas (im)perfeições fala sobre um casamento ", "conturbado e uma promessa esquecida que pode ser capaz ", "de salvá-lo. " };
                 string[] categoria = { "Romance, Aventura, Relaxante" };
                 string[] capitulo = { "" };
 
